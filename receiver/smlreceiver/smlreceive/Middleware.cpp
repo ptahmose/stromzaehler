93a98c17988149ee65f64acac96f7a778ed9f8d5
Juergen Bohl <jbohl@h-quer.de>
2022-01-05 14:56:27 +0100

    Update Middleware.cpp

--- receiver/smlreceiver/smlreceive/Middleware.cpp
+++ receiver/smlreceiver/smlreceive/Middleware.cpp
@@ -8,11 +8,31 @@
 
 using namespace std;
 
+void
+set_blocking(int fd, bool should_block)
+{
+	struct termios tty;
+	memset(&tty, 0, sizeof tty);
+	if (tcgetattr(fd, &tty) != 0)
+	{
+		fprintf(stderr, "error %d from tggetattr", errno);
+		return;
+	}
+
+	tty.c_cc[VMIN] = should_block ? 1 : 0;
+	tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+	if (tcsetattr(fd, TCSANOW, &tty) != 0)
+	{
+		fprintf(stderr, "error %d setting term attributes", errno);
+	}
+}
+
 int main()
 {
 	//FILE* fp = fopen("/dev/ttyACM0", "rb");
 	int fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_SYNC);
-
+	set_blocking(fd, true);
 	for (;;)
 	{
 		char c;
