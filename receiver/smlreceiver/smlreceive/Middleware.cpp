10bbbb06a6e60b72c8beb5446912bc5dfe4e7626
Juergen Bohl <jbohl@h-quer.de>
2022-01-05 14:46:00 +0100

    ...

--- receiver/smlreceiver/smlreceive/Middleware.cpp
+++ receiver/smlreceiver/smlreceive/Middleware.cpp
@@ -2,21 +2,27 @@
 //
 
 #include "Middleware.h"
+#include <fcntl.h> 
+#include <unistd.h>
+#include <termios.h>
 
 using namespace std;
 
 int main()
 {
-	FILE* fp = fopen("/dev/ttyACM0", "rb");
+	//FILE* fp = fopen("/dev/ttyACM0", "rb");
+	int fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_SYNC);
 
 	for (;;)
 	{
-		int c = fgetc(fp);
-		if (c != EOF)
+		char c;
+		int n = read(fd, &c, 1);
+		if (n > 0)
 		{
-			printf("%i", c);
+			printf("%i\n", c);
 		}
 	}
+
 
 	return 0;
 }
