831788785676b7de076b4e02c8bf0c27dd812221
Juergen Bohl <jbohl@h-quer.de>
2022-01-05 14:27:08 +0100

    initial checkin

---
+++ receiver/smlreceiver/smlreceive/Middleware.cpp
@@ -0,0 +1,22 @@
+﻿// Middleware.cpp : Defines the entry point for the application.
+//
+
+#include "Middleware.h"
+
+using namespace std;
+
+int main()
+{
+	FILE* fp = fopen("/dev/ttyACM0", "rb");
+
+	for (;;)
+	{
+		int c = fgetc(fp);
+		if (c != EOF)
+		{
+			fputc(c, stdout);
+		}
+	}
+
+	return 0;
+}
