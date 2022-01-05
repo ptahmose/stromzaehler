21d81e8f2cbfae1b77c670d109fa4b8e736fbe47
Juergen Bohl <jbohl@h-quer.de>
2022-01-05 16:22:08 +0100

    Update ReadMessage.cpp

--- receiver/smlreceiver/smlreceive/ReadMessage.cpp
+++ receiver/smlreceiver/smlreceive/ReadMessage.cpp
@@ -49,14 +49,18 @@ CReadMessage::ReadMessage(Message& message)
 			return 0;
 		}
 
-		this->buffer[bytesRead] = c;
-		++bytesRead;
-		if (bytesRead == this->maxBufferSize)
+		if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' ||
+			c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f' || c == 'A' || c == 'B' || c == 'C' || c == 'D' || c == 'E' || c == 'F')
 		{
-			message = this->ConvertBufferToRawBuffer(bytesRead);
-			/*message.size = bytesRead;
-			message.data = this->buffer;*/
-			return 0;
+			this->buffer[bytesRead] = c;
+			++bytesRead;
+			if (bytesRead == this->maxBufferSize)
+			{
+				message = this->ConvertBufferToRawBuffer(bytesRead);
+				/*message.size = bytesRead;
+				message.data = this->buffer;*/
+				return 0;
+			}
 		}
 	}
 }
