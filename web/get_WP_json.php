6c7c3f8b9ab1142e2944f3c1f6a10dee341ed5d8
Juergen Bohl <jbohl@h-quer.de>
2022-01-06 17:42:42 +0100

    update to website

--- web/get_WP_json.php
+++ web/get_WP_json.php
@@ -1,6 +1,10 @@
 <?php
 $myfile = fopen("/mnt/RAMDisk/zaehlerdata/stromzaehler.txt", "r") or die("Unable to open file!");
-echo fgets($myfile);
-fclose($myfile);
+if (flock($myfile,LOCK_SH))
+{
+    echo fgets($myfile);
+    flock($myfile,LOCK_UN);
+    fclose($myfile);
+}
 // echo "{\"WP_Pges\": 1705,\"WP_Wges\": 1234.5678}";
 ?>
